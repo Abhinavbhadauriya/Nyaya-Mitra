06363e898a24acdfd5df93d07c8b32401bfa122a
Aadarsh <aadarshshrivastava66@gmail.com>
2025-12-06 11:05:57 +0530

    LogIn Improvement

--- middleware.js
+++ middleware.js
@@ -1,6 +1,14 @@
 module.exports.isLogin = (req, res, next) => {
     if (!req.isAuthenticated()) {  
+        req.session.redirectUrl=req.originalUrl;
         return res.redirect('/user/login');
     }
     next(); 
 };
+
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
+    }
+    next();
+}
--- routes/user.js
+++ routes/user.js
@@ -2,22 +2,24 @@ const express=require("express");
 const router=express.Router();
 const user=require('../models/user');
 const passport=require('passport')
-const { isLogin } = require("../middleware");
+const { isLogin, saveRedirectUrl } = require("../middleware");
 
 router.get("/",(req,res)=>{
     res.render("user/verify")
 });
 
-router.get("/signup",(req,res)=>{
-    let {code}=req.query;
+router.get("/signup", (req, res) => {
+    let { code } = req.query;
     console.log(code);
-    if(code==process.env.CODE){
-        res.render("user/newuser");
-    }else{
-        res.send("Invalid code");
-    }
     
-})
+    if (code === process.env.CODE) {
+        res.render("user/newuser");
+    } else {
+        req.flash("error", "Please enter Valid Code");
+        res.redirect("/user");
+    }
+});
+
 
 //save user
 router.post('/register', async (req, res) => {
@@ -27,12 +29,18 @@ router.post('/register', async (req, res) => {
     
     const newUser = new user({ username, email, number });
 
-    await user.register(newUser, password);
-    res.redirect("/");
-    console.log(newUser);
+    const registerUser=await user.register(newUser, password);
+    req.logIn(registerUser,(err)=>{
+      if(err){
+       return res.redirect('/user');
+      }
+      req.flash("success","welcome");
+      res.redirect("/");
+    })
   } catch (err) {
     console.error(err);
-    res.status(400).send(err.message);
+   req.flash("error","user already present");
+   res.redirect("/user");
   }
 });
 
@@ -41,14 +49,15 @@ router.get("/login",(req,res)=>{
 })
 
 router.post("/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureFlash: true,
     failureRedirect: "/user/login",
   }),
-  (req, res) => {
-    
+  (req, res) => {    
     req.flash("success","Welcome")
-    res.redirect("/"); 
+    let redirectUrl=res.locals.redirectUrl||"/";
+    res.redirect(redirectUrl); 
   }
 );
 
